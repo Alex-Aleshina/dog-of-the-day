83fd67baacb88656d874b80d4be3df7385c2a3e7
Alex-Aleshina <aleksandra@wirelab.nl>
2020-05-26 16:33:58 +0200

    fix bug with empty chosen breed

--- src/components/OneDogPage.vue
+++ src/components/OneDogPage.vue
@@ -1,10 +1,11 @@
 <template>
 <div class="OneDogPage">
-    <div class="flex flex-col justify-center items-center">
+    <div class="flex flex-col justify-center items-center" v-if="chosenBreed.length > 0">
         <div>
             <button v-on:click='reloadImg' class=" focus:outline-none m-6 bg-green-700 hover:shadow-2xl hover:bg-green-500 active:bg-green-900 text-white font-bold py-2 px-4 border-green-600 shadow-lg rounded w-64 tracking-wider">Fetch new image</button>
         </div>
-        <div v-if="chosenBreed" class="px-8 py-4 max-w-4xl w-full">
+
+        <div class="px-8 py-4 max-w-4xl w-full">
             <img v-bind:src='info' alt='dog' class="imgOfDog rounded-md overflow-hidden shadow-2xl">
         </div>
     </div>
